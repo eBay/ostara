f52704105e893580603d729c09ac5dad17ecc958
renyedi <renyedi@ebay.com>
2015-05-06 13:02:07 -0700

    Initial commit: a simple web driver for ostara-service

---
+++ ostara-web/src/main/java/org/ostara/web/OstaraController.java
@@ -0,0 +1,22 @@
+package org.ostara.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class OstaraController {
+	  private static Logger logger = LoggerFactory.getLogger(OstaraController.class);
+
+	  @RequestMapping(value="upgrade", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+	  @ResponseBody
+	  public ModelAndView upgrade() {
+	    return new ModelAndView("showMessage");
+	  }
+
+}
